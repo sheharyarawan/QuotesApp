96abc80d674c0e4ecc9d526b9a1f704be2ef3e10
Sheharyar Awan <sheharyarawan1153@gmail.com>
2026-06-01 15:27:59 +0500

    Quote List Item ui created

---
+++ app/src/main/java/com/example/quotesapp/screens/QuoteListItem.kt
@@ -0,0 +1,60 @@
+package com.example.quotesapp.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FormatQuote
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.quotesapp.model.Quote
+
+@Composable
+fun QuoteListItem(quote: Quote) {
+    Card(modifier = Modifier.padding(8.dp),
+    elevation = CardDefaults.cardElevation(
+        defaultElevation = 4.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        )
+    )
+       {
+        Row(modifier = Modifier.padding(16.dp)) {
+            Image(
+                imageVector = Icons.Filled.FormatQuote,
+                contentDescription = "",
+                colorFilter = ColorFilter.tint(Color.White),
+                modifier= Modifier.background(Color.Black).
+                size(40.dp).
+                rotate(180F)
+            )
+            Spacer(modifier= Modifier.padding(4.dp))
+            Column() {
+                Text(text = quote.text,
+                    style = MaterialTheme.typography.titleSmall,
+                    modifier = Modifier.padding(0.dp, 0.dp,0.dp,8.dp))
+                Box(modifier = Modifier.height(1.dp).
+                background(Color.Gray).
+                fillMaxWidth(0.4f))
+                Text(text = quote.author,
+                    modifier= Modifier.padding(0.dp,4.dp,0.dp,0.dp))
+            }
+        }
+    }
+}
